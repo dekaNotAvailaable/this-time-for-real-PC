9269b87dcbed92e47479a229dd79cda284be1c19
MemerUnknown <MemerUnknown@users.noreply.github.com>
2023-09-07 11:35:44 +0200

    fixed version of this shit

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,35 +2,45 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEngine.UIElements;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
     bool ready = true;
     bool loaded = true;
     int rounds = 12;
-    float rate = 1 / 2f;
+    float rate = 1/2f;
     float last;
     float loadspeed = 3f;
     float Loading;
     bool loader;
+    public Text Ammo;
     private void Start()
     {
-
+     
     }
+
     void Update()
     {
+       
+        Ammo.text = rounds.ToString();
         firespeed();
-        if (ready == true && loaded == true && Input.GetMouseButton(0))
-        {
-            FireRay();
-            rounds -= 1;
-            ready = false;
-            Debug.Log("fired");
-        }
+        if (ready == true && loaded == true && Input.GetMouseButton(0)) 
+      {
+       FireRay();
+       rounds -= 1;
+       ready = false;
+       Debug.Log("fired");
+       }
+
+    
+
         if (Input.GetKey("r"))
         {
             loader = true;
+            loaded = false;
         }
         Debug.Log("loading =" + Loading);
         if (loader == true)
@@ -46,19 +56,28 @@ public class Gun : MonoBehaviour
             loader = false;
             Loading = 0;
         }
+
+
         if (rounds <= 0)
-        {
-            loaded = false;
-        }
+            {
+                loaded = false;
+            }
+       
+
     }
 
     void FireRay()
     {
+        
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
+
             hit.collider.gameObject.GetComponent<Fentanyl>()?.TakeDamage(1);
+                
+            
         }
+
     }
 
     void firespeed()
