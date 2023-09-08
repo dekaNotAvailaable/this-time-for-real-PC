6a5a58ad864fac256d0a302cf2d36bfaaf9c98ce
dekaNotAvailaable <125357384+dekaNotAvailaable@users.noreply.github.com>
2023-09-08 09:51:43 +0200

    showcase for ppt

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EnemyHealthSatuts : MonoBehaviour
+public class EnemyBehaviour : MonoBehaviour
 {
     public Image red;
     public Image green;
@@ -11,55 +11,71 @@ public class EnemyHealthSatuts : MonoBehaviour
     public Image orange;
     private float timeFollower;
     [SerializeField]
-    public float greenMax, greenMin,redMax,redMin,orangeMin,orangeMax,yellowMin,yellowMax;
+    public float greenMax, greenMin, redMax, redMin, orangeMin, orangeMax, yellowMin, yellowMax;
     private int transformCount;
-    public int _transformCount()
-    {
-        return transformCount;
-    }
+    public MeshRenderer renderOne;
+    public MeshRenderer renderTwo;
+    public MeshRenderer renderThree;
+
 
 
     void Start()
     {
         timeFollower = Time.time;
+        renderOne.enabled = true;
+        renderTwo.enabled = false;
+        renderThree.enabled = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         ColorChanges();
         Debug.Log(transformCount);
+        EnemyTransform();
     }
     void ColorChanges()
     {
         if (Time.time >= timeFollower + 1)
         {
-            
+
             timeFollower = Time.time;
             green.fillAmount -= Random.Range(greenMin, greenMax);
             if (green.fillAmount <= 0)
             {
                 yellow.fillAmount -= Random.Range(yellowMin, yellowMax);
                 green.fillAmount = 0;
-                transformCount= 1;
+                transformCount = 1;
             }
             if (yellow.fillAmount <= 0)
             {
                 orange.fillAmount -= Random.Range(orangeMin, orangeMax);
                 yellow.fillAmount = 0;
-                transformCount= 2;
+                transformCount = 2;
             }
             if (orange.fillAmount <= 0)
             {
                 red.fillAmount -= Random.Range(redMin, redMax);
                 orange.fillAmount = 0;
-                transformCount= 3;
+                transformCount = 3;
             }
             if (red.fillAmount <= 0)
             {
                 red.fillAmount = 0;
             }
         }
-
+    }
+    void EnemyTransform()
+    {
+        if (transformCount == 1)
+        {
+            renderOne.enabled = false;
+            renderTwo.enabled = true;
+        }
+        else if (transformCount == 2)
+        {
+            renderTwo.enabled = false;
+            renderThree.enabled = true;
+        }
     }
 }
