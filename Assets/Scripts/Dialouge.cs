2cb85bb3737ffb4f7d7ed392a4ae44fe8fca0230
dekaNotAvailaable <125357384+dekaNotAvailaable@users.noreply.github.com>
2023-09-12 17:07:07 +0200

    df

--- Assets/Scripts/Dialouge.cs
+++ Assets/Scripts/Dialouge.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
-    {
+{
     public TextMeshProUGUI textComponent;
     public float textSpeed;
     private bool choiceToggle;
@@ -14,73 +14,73 @@ public class Dialogue : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-        {
+    {
         for (int i = 0; i < buttons.Length; i++)
-            {
+        {
             int mysteryCopy = i;
             // TODO: during C++ labs, revisit this (capture lists...)
             // or google: stackoverflow: C# lambda capture a copy instead of reference
             buttons[i].onClick.AddListener(() => { Button1Clicked(mysteryCopy); });
-            }
-        StartDialogue();
         }
+        StartDialogue();
+    }
 
     void Button1Clicked(int buttonNumber)
-        {
+    {
         Debug.Log("Button clicked: " + buttonNumber);
-        }
+    }
 
     // Update is called once per frame
     void Update()
-        {
+    {
         if (Input.GetMouseButtonDown(1))
-            {
+        {
             NextLine();
-            }
-        if (Input.GetKeyUp(KeyCode.T))
-            {
-            choiceToggle = !choiceToggle;
-            }
         }
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            choiceToggle = !choiceToggle;
+        }
+    }
 
     void StartDialogue()
-        {
+    {
         dialogueIndex = 0;
         choiceToggle = false;
         currentLines = OriginalLines();
         StartCoroutine(TypeLine());
-        }
+    }
 
     IEnumerator TypeLine()
-        {
+    {
         if (dialogueIndex < currentLines.Length)
-            {
+        {
             textComponent.text = string.Empty;
             foreach (char c in currentLines[dialogueIndex].ToCharArray())
-                {
+            {
                 textComponent.text += c;
                 yield return new WaitForSeconds(textSpeed);
-                }
-            dialogueIndex++;
             }
+            dialogueIndex++;
+        }
         else
-            {
+        {
             choiceToggle = !choiceToggle;
             dialogueIndex = 0;
             currentLines = choiceToggle ? GetLinesForChoice1() : GetLinesForChoice2();
             StartCoroutine(TypeLine());
-            }
         }
+    }
 
     void NextLine()
-        {
+    {
         if (dialogueIndex >= currentLines.Length - 1)
-            {
-            StartCoroutine(TypeLine());
-            }
-        }
-    public string[] OriginalLines()
         {
+            StartCoroutine(TypeLine());
+        }
+    }
+    public string[] OriginalLines()
+    {
         return new string[]
         {
             "orignal line 1",
@@ -92,22 +92,22 @@ public class Dialogue : MonoBehaviour
 
 
 
-        }
+    }
     public string[] GetLinesForChoice1()
-        {
+    {
         return new string[]
         {
             "Choice 1 Line 1",
             "Choice 1 Line 2",
         };
-        }
+    }
 
     public string[] GetLinesForChoice2()
-        {
+    {
         return new string[]
         {
             "Choice 2 Line 1",
             "Choice 2 Line 2",
         };
-        }
     }
+}
