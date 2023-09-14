3fcd00c4b353feea09648505e3c3f11ef7de9c02
MemerUnknown <MemerUnknown@users.noreply.github.com>
2023-09-14 13:23:56 +0200

    working until merge dk
    This reverts commit 1f3e2a428a83ac75e0e95af6906e3b6c9412ebd6.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -25,15 +25,12 @@ public class SC_FPSController : MonoBehaviour
         rb= GetComponent<Rigidbody>();
 
         // Lock cursor
-<<<<<<< Updated upstream
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
-=======
-       Cursor.lockState = CursorLockMode.Locked;
-         Cursor.visible = false;
-        }
->>>>>>> Stashed changes
+
+
 
     void Update()
     {
