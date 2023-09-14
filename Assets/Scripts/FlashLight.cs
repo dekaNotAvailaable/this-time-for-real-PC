af35695289b3794cd4dafe5b66f0840e9de48106
dekaNotAvailaable <125357384+dekaNotAvailaable@users.noreply.github.com>
2023-09-15 00:17:19 +0200

    phone and flashlight

--- Assets/Scripts/FlashLight.cs
+++ Assets/Scripts/FlashLight.cs
@@ -1,22 +1,23 @@
 using UnityEngine;
 
-public class FlashLight :MonoBehaviour
-    {
+public class FlashLight : MonoBehaviour
+{
     // Start is called before the first frame update
     public Light flashLight;
     private bool onOffSwitch;
     void Start()
-        {
-
-        }
+    {
+        flashLight.color = Color.yellow;
+        flashLight.intensity = 10f;
+    }
 
     // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.V))
         {
-        if(Input. GetKeyUp(KeyCode. V))
-            {
             onOffSwitch = !onOffSwitch;
-            }
-        flashLight. enabled = onOffSwitch ? true : false;
         }
+        flashLight.enabled = onOffSwitch ? true : false;
     }
+}
